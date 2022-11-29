a152d638e7852a98eb873103fa9ecabdf7db4978
KenichiQaz <31177857+KenichiQaz@users.noreply.github.com>
2022-11-29 11:44:40 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from moviepy.editor import *
+   
+def in_outro():
+    inoutro=
+
+def split_file(original_file):
+    if file is not None:
+        clip = VideoFileClip(original_file)
+
+    final_clip=moviepy.editor.concatenate_videoclips([inoutro,clip1,inoutro])
+    final_clip.write_videofile("output_1.mp4", codec='libx264')
+
+def editfile():
+    in_outro()
+  
+if __name__ == "__main__":
+    editfile()
