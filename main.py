8bd24ff21a651def62d2b564bda1bc4b940e3752
KenichiQaz <31177857+KenichiQaz@users.noreply.github.com>
2023-03-09 09:42:21 +0700

    Added system arguments handling

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from moviepy.editor import *
+import sys
 
-INTRO=""
-OUTRO=""
-
-def Merge_Files(original_file):
-    if file is not None:
-        clip = VideoFileClip(original_file)
-        final_clip=moviepy.editor.concatenate_videoclips([INTRO,clip1,OUTRO])
-        final_clip.write_videofile("output_1.mp4", codec='libx264')
-    else:
-        print("No file")
+class Video:
+    def __init__(self, intro, video, outro):
+        self.intro = intro
+        self.video = video
+        self.outro = outro
         
+    def Merge_Files(original_file):
+        if file is not None:
+            clip = VideoFileClip(original_file)
+            final_clip=moviepy.editor.concatenate_videoclips([INTRO,clip1,OUTRO])
+            final_clip.write_videofile("output_1.mp4", codec='libx264')
+        else:
+            print("No file")
+
 if __name__ == "__main__":
-    Merge_Files("input.mp4")
+    if sys.argv[3] == "":
+        video.Merge_Files(sys.argv[1],sys.argv[2],sys.argv[1])
+    elif sys.argv[3] != "":
+        video.Merge_Files(sys.argv[1],sys.argv[2],sys.argv[3])
