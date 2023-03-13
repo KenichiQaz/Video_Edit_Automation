766e520257cc39d7ba85df7ba13456a6ecbe1800
KenichiQaz <31177857+KenichiQaz@users.noreply.github.com>
2023-03-13 01:39:35 +0000

    Updated project for public release

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Video_Edit_Automation
-Video editing automation and adding intro and outro
+<h1 align="center">Welcome to Video_Edit_Automation</h1>
+<p>
+</p>
+
+> This project automatically adds the intro and outro files to the video.
+
+## Author
+
+👤 **Stefan Enslin**
+
+* Github: [@KenichiQaz](https://github.com/KenichiQaz)
+* LinkedIn: [@stefanenslin](https://linkedin.com/in/stefanenslin)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from moviepy.editor import *
-import sys
+from sys import argv
 
 class Video:
     def __init__(self, intro, video, outro):
@@ -10,13 +10,13 @@ class Video:
     def Merge_Files(original_file):
         if file is not None:
             clip = VideoFileClip(original_file)
-            final_clip=moviepy.editor.concatenate_videoclips([INTRO,clip1,OUTRO])
+            final_clip=moviepy.editor.concatenate_videoclips([INTRO,clip,OUTRO])
             final_clip.write_videofile("output_1.mp4", codec='libx264')
         else:
             print("No file")
 
 if __name__ == "__main__":
-    if sys.argv[3] == "":
-        video.Merge_Files(sys.argv[1],sys.argv[2],sys.argv[1])
-    elif sys.argv[3] != "":
-        video.Merge_Files(sys.argv[1],sys.argv[2],sys.argv[3])
+    if argv[3] == "":
+        video.Merge_Files(argv[1],argv[2],argv[1])
+    elif argv[3] != "":
+        video.Merge_Files(argv[1],argv[2],argv[3])
