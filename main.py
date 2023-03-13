48adc42301789ca5e86113efcc78652aab8afe87
KenichiQaz <31177857+KenichiQaz@users.noreply.github.com>
2023-03-13 01:49:01 +0000

    Update project for publication

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-from moviepy.editor import *
 from sys import argv
+from moviepy.editor import *
 
-class Video:
-    def __init__(self, intro, video, outro):
-        self.intro = intro
-        self.video = video
-        self.outro = outro
-        
-    def Merge_Files(original_file):
-        if file is not None:
-            clip = VideoFileClip(original_file)
-            final_clip=moviepy.editor.concatenate_videoclips([INTRO,clip,OUTRO])
-            final_clip.write_videofile("output_1.mp4", codec='libx264')
-        else:
-            print("No file")
+def Merge_Files(original_file, intro_file, outro_file):
+    '''This function merges the intro and outro files with the original file.'''
+    if original_file is not None:
+        clip = VideoFileClip(original_file)
+        final_clip = concatenate_videoclips([intro_file,clip,outro_file])
+        final_clip.write_videofile("output_1.mp4", codec='libx264')
+    else:
+        print("No file")
 
 if __name__ == "__main__":
     if argv[3] == "":
-        video.Merge_Files(argv[1],argv[2],argv[1])
+        Merge_Files(argv[1],argv[2],argv[1])
     elif argv[3] != "":
-        video.Merge_Files(argv[1],argv[2],argv[3])
+        Merge_Files(argv[1],argv[2],argv[3])
